5f7272b0906bc619101eede6f77e32b83fe5ab56
Conor Clyde <CLY16100551@live.nwrc.ac.uk>
2021-03-09 13:55:25 +0000

    ...

---
+++ PlacementApp/PlacementApp/PlacementApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PlacementApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PlacementApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PlacementApp/PlacementApp/PlacementApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PlacementApp.Models;
+using PlacementApp.ViewModels;
+using PlacementApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlacementApp.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
