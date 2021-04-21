3563e9bf931e569758c2350ca216957ddf75d8dd
Conor Clyde <CLY16100551@live.nwrc.ac.uk>
2021-04-21 14:40:45 +0100

    ,,,

--- PlacementApp/PlacementApp/PlacementApp/Utils/DummyScheduleData.cs
+++ PlacementApp/PlacementApp/PlacementApp/Utils/DummyScheduleData.cs
@@ -11,8 +11,6 @@ namespace PlacementApp.Utils
             var data = new List<Schedule>();
             var person = new Schedule()
             {
-                ScheduleID = "1",
-                PlacementID = "1",
                 WorkDate = "2020-09-04",
                 WorkStartTime = "09:00:00.0000000",
                 WorkEndTime = "14:00:00.0000000"
