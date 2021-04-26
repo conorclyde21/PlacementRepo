aa41a6075002e3b316ceef2e1f8cf086b664e8de
Conor Clyde <CLY16100551@live.nwrc.ac.uk>
2021-04-26 16:08:09 +0100

    ,,,

--- PlacementApp/PlacementApp/PlacementApp/App.xaml.cs
+++ PlacementApp/PlacementApp/PlacementApp/App.xaml.cs
@@ -31,7 +31,7 @@ namespace PlacementApp
 
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainMenu());
+            MainPage = new NavigationPage(new LoginPage());
 
         }
 
--- PlacementApp/PlacementApp/PlacementApp/Views/LoginPage.xaml.cs
+++ PlacementApp/PlacementApp/PlacementApp/Views/LoginPage.xaml.cs
@@ -3,6 +3,8 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using PlacementApp.Data;
 using PlacementApp.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace PlacementApp.Pages
 {
@@ -10,13 +12,13 @@ namespace PlacementApp.Pages
     public partial class LoginPage : ContentPage
     {
 
-        PlacementDatabaseControllerConstants userData;
+        PlacementDatabaseController userData;
 
         public LoginPage()
         {
             InitializeComponent();
             Init();
-            userData = new PlacementDatabaseControllerConstants();
+            userData = new PlacementDatabaseController();
         }
 
 
@@ -27,12 +29,59 @@ namespace PlacementApp.Pages
             Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
         }
 
+
+        string Conn = ("Data Source=LAPTOP-GLNNG57B\\SQLEXPRESS01;Initial Catalog=TestDatabaseFI;Integrated Security=True");
+
+
         private void SignInProcedure(object sender, EventArgs e)
         {
+            try
+            {
 
+                if (Entry_Username.Text == null && Entry_Password.Text == null)
+                {
+                    DisplayAlert("Login", "Please Enter The User Name & Password", "Ok");
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(Conn);
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM [User] where Username=@UserName and Password=@Password", con);
+                    cmd.Parameters.AddWithValue("@UserName", Entry_Username.Text);
+                    cmd.Parameters.AddWithValue("@Password", Entry_Password.Text);
+
+                    con.Open();
+                    SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adpt.Fill(ds);
+                    con.Close();
+
+                    int count = ds.Tables[0].Rows.Count;
+
+                    if (count == 1)
+                    {
+                        App.Current.MainPage.Navigation.PushAsync(new MainMenu());
+                    }
+                    else
+                    {
+                        DisplayAlert("Login", "Invalid Username & Password", "Ok");
+                    }
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                DisplayAlert("Login", ex.Message, "Ok");
+            }
+
+
+
+
+            /*
             if (Entry_Username.Text != null && Entry_Password.Text != null)
             {
-                var validData = userData.LoginValidateConstants(Entry_Username.Text, Entry_Password.Text);
+                var validData = userData.LoginValidate(Entry_Username.Text, Entry_Password.Text);
                 if (validData)
                 {
 
@@ -49,6 +98,7 @@ namespace PlacementApp.Pages
                 
                 DisplayAlert("Empty Fields", "Enter User Name and Password Please", "OK");
             }
+            */
         }
 
     }
